529bf2e717e134343eb82a4e176504dad418d193
MateoTVara <146038574+MateoTVara@users.noreply.github.com>
2025-08-23 21:46:48 -0500

    Refactor CSS: add design tokens and tidy form styles

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const inputPassword = bind("#password");
 const inputConfirmPassword = bind("#confirm_password");
 
 
-btnSubmit.addEventListener("click", (e) => {
+btnSubmit.addEventListener("click", () => {
   const password = inputPassword.value;
   const confirmPassword = inputConfirmPassword.value;
 
