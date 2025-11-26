28051d26332a37530fba1abbf1588ff8eb7fc46b
MateoTVara <146038574+MateoTVara@users.noreply.github.com>
2025-11-26 09:17:02 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('node:path');
+const { env } = require('node:process');
+
+require('dotenv').config();
+
+const { LISTENING_PORT } = env;
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const fallbackPort = 3000;
+
+app.listen(LISTENING_PORT || fallbackPort, (error) => {
+  if (error) throw error
+  console.log(`Listening on port ${LISTENING_PORT || fallbackPort}`)
+});
