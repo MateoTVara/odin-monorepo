80a52d5d5cbb006903342df000fe34200900a13e
MateoTVara <146038574+MateoTVara@users.noreply.github.com>
2025-12-12 08:49:05 -0500

    modifies pool to use external render url for database connection

--- db/pool.js
+++ db/pool.js
@@ -1,15 +1,20 @@
 const { Pool } = require('pg');
 const { env } = require('node:process');
 
-require('dotenv').config();
-const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD, DB_PORT } = env;
+// load .env only when not in production
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD, DB_PORT, CONNECTION_STRING } = env;
 
-const pool = new Pool({
-  host: DB_HOST,
-  user: DB_USER,
-  database: DB_NAME,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-});
+const pool = CONNECTION_STRING
+  ? new Pool({ connectionString: CONNECTION_STRING, ssl: { rejectUnauthorized: false } })
+  : new Pool({
+      host: DB_HOST,
+      user: DB_USER,
+      database: DB_NAME,
+      password: DB_PASSWORD,
+      port: DB_PORT ? Number(DB_PORT) : undefined,
+    });
 
 module.exports = pool;
